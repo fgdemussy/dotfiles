3f0b420bfa8b705c1078a0e58a5616748a3dc45c
Fran <fuat@Fuats-MacBook-Pro.local>
2024-05-29 08:46:46 -0400

    Adds starship, zsh, and zsh setup.sh

---
+++ zsh/setup.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+setup_color() {
+    # Only use colors if connected to a terminal
+    if [ -t 1 ]; then
+        RED=$(printf '\033[31m')
+        GREEN=$(printf '\033[32m')
+        YELLOW=$(printf '\033[33m')
+        BLUE=$(printf '\033[34m')
+        BOLD=$(printf '\033[1m')
+        RESET=$(printf '\033[m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        RESET=""
+    fi
+}
+
+vim_syntax_on() {
+    echo "syntax on" >>~/.vimrc
+}
+
+install_zsh() {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+}
+
+install_plugins() {
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sed -i"_postinstall" '/^plugins=/N;s/(git)/(git zsh-navigation-tools zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+}
+
+install_terminal_tools() {
+    brew install zoxide eza fzf
+}
+
+main() {
+    # Get the directory where the script is located
+    script_dir=$(dirname "$(realpath "$0")")
+
+    setup_color
+
+    install_zsh
+
+    if [[ $? -eq 0 ]]; then
+        vim_syntax_on
+        install_plugins
+        install_terminal_tools
+        zsh
+        mv ~/.zshrc ~/.zshrc.bak
+        ln -s "$(script_dir)/.zshrc" ~/.zshrc
+    else
+        echo "${YELLOW}Installing Oh-my-zsh failed."
+        exit 1
+    fi
+}
+
+main "$@"
