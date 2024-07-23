24adc73e1c103ec278c029f659ed5341929ae736
Fran <fgdemussy@gmail.com>
2024-07-22 22:42:32 -0400

    chore: make wezterm opacity 1

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,6 @@ return {
 	enable_tab_bar = false,
 	font_size = 14.0,
 	font = wezterm.font("JetBrains Mono"),
-	window_background_opacity = 1,
+	-- window_background_opacity = 0.8,
 	window_decorations = "RESIZE",
 }
