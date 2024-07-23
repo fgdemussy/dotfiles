6eb6c842fcb2bc68015f9b884673dd369dee968f
Fran <fgdemussy@gmail.com>
2024-07-22 23:22:17 -0400

    wezterm colorscheme based on os

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,15 @@
 local wezterm = require("wezterm")
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 return {
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	enable_tab_bar = false,
 	font_size = 14.0,
 	font = wezterm.font("JetBrains Mono"),
