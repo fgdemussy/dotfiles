5af28ec5c5028b36ac5727de1fbeb292f5707d1c
François Gueneau de Mussy <fgdemussy@gmail.com>
2025-07-17 20:36:19 -0300

    flash.nvim enable rainbox for better light theme color contrast

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    label = {
+      -- Enable this to use rainbow colors to highlight labels
+      -- Can be useful for visualizing Treesitter ranges.
+      rainbow = {
+        enabled = true,
+        -- number between 1 and 9
+        shade = 3,
+      },
+    },
+  },
+}
